c261623152d5fe2412a98166e75d8de38fe90349
patte <p@tte.io>
2023-12-09 03:22:49 +0100

    day8 part1

---
+++ day8/src/main.rs
@@ -0,0 +1,102 @@
+use std::collections::HashMap;
+
+/*
+https://adventofcode.com/2023/day/8
+*/
+#[allow(dead_code)]
+static EXAMPLE: &str = "\
+RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)";
+
+// 🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄
+// this could be solved with a hash map and made fast with a tree
+// for now only the hash map is implemented, as it solves the example in 0.01s
+//
+// nice tree library:
+// https://github.com/saschagrunert/indextree
+
+fn main() {
+    println!("Hello, adventofcode.com/2023/day/8 from rust!");
+    let args = std::env::args().collect::<Vec<String>>();
+    let part = if args.len() > 1 {
+        args[1].parse::<u64>().unwrap()
+    } else {
+        1
+    };
+    println!("--- Part {} ---", if part == 1 { "One" } else { "Two" });
+
+    //let input = EXAMPLE;
+    let input = std::fs::read_to_string("input.txt").unwrap();
+
+    let mut lines = input.lines();
+
+    // LRLLR (round robin)
+    let directions = lines.next().unwrap();
+    println!("{}", directions);
+
+    let mut map_entries = HashMap::new();
+    for line in lines {
+        if line.is_empty() {
+            continue;
+        }
+        // println!("{}", line);
+
+        // parse line
+        let (node, (edge_left, edge_right)) = line
+            .split_once('=')
+            .map(|(n, d)| {
+                let edges = d
+                    .trim()
+                    .replace("(", "")
+                    .replace(",", "")
+                    .replace(")", "")
+                    .split_once(' ')
+                    .map(|(l, r)| (l.to_string(), r.to_string()))
+                    .unwrap();
+                (n.trim().to_string(), edges)
+            })
+            .unwrap();
+
+        // store in map
+        map_entries.insert(node, (edge_left, edge_right));
+    }
+    // println!("{:#?}", map_entries);
+
+    let mut current_node = "AAA";
+    let mut step_count = 0;
+    for direction_index in (0..directions.len()).cycle() {
+        match map_entries.get(current_node) {
+            Some((edge_left, edge_right)) => {
+                let direction = directions.chars().nth(direction_index).unwrap();
+                print!("{}-{}->", current_node, direction);
+                match direction {
+                    'L' => {
+                        current_node = edge_left;
+                    }
+                    'R' => {
+                        current_node = edge_right;
+                    }
+                    _ => {
+                        panic!("Unknown direction {}", direction);
+                    }
+                }
+                step_count += 1;
+                println!("{} {}", current_node, step_count);
+            }
+            None => {
+                panic!("{current_node} not found in map!")
+            }
+        }
+        if current_node == "ZZZ" {
+            break;
+        }
+    }
+    println!("{} steps", step_count);
+}
