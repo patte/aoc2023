f2d717bf5f6ce87f2a242f9e460022300698eb76
patte <p@tte.io>
2023-12-09 21:19:43 +0100

    day8 part2

--- day8/README.md
+++ day8/README.md
@@ -21,12 +21,82 @@ AAA-[L]->FHJ 1
 FHJ-[R]->TGV 2
 TGV-[L]->PVS 3
 PVS-[R]->TJC 4
-...
-22199 steps
-target/release/day8  0.01s user 0.02s system 28% cpu 0.101 total
+...    ->ZZZ 22199
+steps: 22199
+target/release/day8 1  0.00s user 0.00s system 4% cpu 0.075 total
 ```
 
 ### Part 2
 ```
-
+✨ 💫 ✨ ⭐️ ✨ 💫 ✨ ✨ 💫 ✨ ⭐️ ✨ 💫 ✨ ✨ 💫
+Hello, adventofcode.com/2023/day/8 from rust!
+--- Part Two ---
+directions: LRLRLRLLRRLRRLRRRLRRLRLLRRRLRRRLRRLLLLRRRLRLLRRLRRLRRLLLRRRLRRRLRRLRLRRLRLRLRLLRRRLRRRLLRRRLRRRLRRRLRLLLRRLRLRRRLRLRRRLLRRRLRLLRLRRRLRLRRRLRRLLRLRLRRLRLRLRRLRLRLRRRLRRLRLLRRLRRRLRRRLRRLRRRLRRLRLRRRLLRRRLLRRLRLRRRLRRRLLRRRLRLRRLRLRLRRLRLLRRLRLRLRRLRRRLRRRLRLRRLRRLLLRRRLLRLRRRLLRRRR
+PXA-[L]->KBG 1
+KBG-[R]->HFS 2
+HFS-[L]->HML 3
+HML-[R]->SVV 4
+...    ->LHZ 14893
+VXA-[L]->BRF 1
+BRF-[R]->LNH 2
+LNH-[L]->SHB 3
+SHB-[R]->SDJ 4
+...    ->TNZ 16579
+DVA-[L]->XXG 1
+XXG-[R]->MVX 2
+MVX-[L]->RBK 3
+RBK-[R]->XMX 4
+...    ->XDZ 13207
+NMA-[L]->KGM 1
+KGM-[R]->DHM 2
+DHM-[L]->RPP 3
+RPP-[R]->VVM 4
+...    ->JFZ 17141
+AAA-[L]->FHJ 1
+FHJ-[R]->TGV 2
+TGV-[L]->PVS 3
+PVS-[R]->TJC 4
+...    ->ZZZ 22199
+JHA-[L]->BXQ 1
+BXQ-[R]->MVV 2
+MVV-[L]->FPF 3
+FPF-[R]->QNX 4
+...    ->FRZ 18827
+ghosts: [Ghost { current_node: "LHZ", step_count: 14893, loop_step_count: 0 }, Ghost { current_node: "TNZ", step_count: 16579, loop_step_count: 0 }, Ghost { current_node: "XDZ", step_count: 13207, loop_step_count: 0 }, Ghost { current_node: "JFZ", step_count: 17141, loop_step_count: 0 }, Ghost { current_node: "ZZZ", step_count: 22199, loop_step_count: 0 }, Ghost { current_node: "FRZ", step_count: 18827, loop_step_count: 0 }]
+max_step_count: 22199
+LHZ-[L]->KQF 14894
+KQF-[R]->HVS 14895
+HVS-[L]->HML 14896
+HML-[R]->SVV 14897
+...    ->LHZ 29786
+         loop!!!
+TNZ-[L]->XKN 16580
+XKN-[R]->VFV 16581
+VFV-[L]->SHB 16582
+SHB-[R]->SDJ 16583
+...    ->TNZ 33158
+         loop!!!
+XDZ-[L]->PRR 13208
+PRR-[R]->FHN 13209
+FHN-[L]->RBK 13210
+RBK-[R]->XMX 13211
+...    ->XDZ 26414
+         loop!!!
+JFZ-[L]->SXH 17142
+SXH-[R]->DHM 17143
+DHM-[L]->RPP 17144
+RPP-[R]->VVM 17145
+...    ->JFZ 34282
+         loop!!!
+skip: Ghost { current_node: "ZZZ", step_count: 22199, loop_step_count: 0 }
+FRZ-[L]->RTP 18828
+RTP-[R]->MVV 18829
+MVV-[L]->FPF 18830
+FPF-[R]->QNX 18831
+...    ->FRZ 37654
+         loop!!!
+ghosts: [Ghost { current_node: "LHZ", step_count: 14893, loop_step_count: 14893 }, Ghost { current_node: "TNZ", step_count: 16579, loop_step_count: 16579 }, Ghost { current_node: "XDZ", step_count: 13207, loop_step_count: 13207 }, Ghost { current_node: "JFZ", step_count: 17141, loop_step_count: 17141 }, Ghost { current_node: "ZZZ", step_count: 22199, loop_step_count: 0 }, Ghost { current_node: "FRZ", step_count: 18827, loop_step_count: 18827 }]
+max_step_count: 22199
+steps: 13334102464297
+target/release/day8 2  0.01s user 0.00s system 12% cpu 0.078 total
 ```
--- day8/src/main.rs
+++ day8/src/main.rs
@@ -4,7 +4,7 @@ use std::collections::HashMap;
 https://adventofcode.com/2023/day/8
 */
 #[allow(dead_code)]
-static EXAMPLE: &str = "\
+static EXAMPLE_PART1: &str = "\
 RL
 
 AAA = (BBB, CCC)
@@ -15,6 +15,20 @@ EEE = (EEE, EEE)
 GGG = (GGG, GGG)
 ZZZ = (ZZZ, ZZZ)";
 // answer: 2
+
+#[allow(dead_code)]
+static EXAMPLE_PART2: &str = "\
+LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)";
+// answer: 6
 
 // this can be solved with a hash map and made fast with a tree
 // for now only the hash map is implemented, as it solves the example in 0.01s
@@ -59,32 +73,31 @@ fn parse_input(input: &str) -> (String, HashMap<String, (String, String)>) {
     (directions.to_string(), map_entries)
 }
 
-fn main() {
-    println!("✨ 💫 ✨ ⭐️ ✨ 💫 ✨ ✨ 💫 ✨ ⭐️ ✨ 💫 ✨ ✨ 💫");
-    println!("Hello, adventofcode.com/2023/day/8 from rust!");
+fn solve_labirynth(
+    directions: &str,
+    map_entries: &HashMap<String, (String, String)>,
+    start: &str,
+    end: &str,
+    start_direction_index: u64,
+    print_steps: bool,
+) -> (String, u64) {
+    let mut current_node = start;
+    let mut step_count: u64 = 0;
 
-    let args = std::env::args().collect::<Vec<String>>();
-    let part = if args.len() > 1 {
-        args[1].parse::<u64>().unwrap()
-    } else {
-        1
-    };
-    println!("--- Part {} ---", if part == 1 { "One" } else { "Two" });
-
-    //let input = EXAMPLE;
-    let input = std::fs::read_to_string("input.txt").unwrap();
-
-    let (directions, map_entries) = parse_input(&input);
-
-    let mut current_node = "AAA";
-    let mut step_count = 0;
-    for direction in directions.chars().cycle() {
+    for direction in directions.chars().cycle().skip(
+        // modulo is needed because skip wants a usize and start_direction_index is u64
+        (start_direction_index % directions.len() as u64)
+            .try_into()
+            .unwrap(),
+    ) {
         match map_entries.get(current_node) {
             Some((edge_left, edge_right)) => {
-                if step_count < 4 {
-                    print!("{}-[{}]->", current_node, direction);
-                } else if step_count == 4 {
-                    println!("...");
+                if print_steps {
+                    if step_count < 4 {
+                        print!("{}-[{}]->", current_node, direction);
+                    } else if step_count == 4 {
+                        print!("...");
+                    }
                 }
                 match direction {
                     'L' => {
@@ -98,17 +111,145 @@ fn main() {
                     }
                 }
                 step_count += 1;
-                if step_count < 5 {
-                    println!("{} {}", current_node, step_count);
+                if print_steps && step_count < 5 {
+                    println!("{} {}", current_node, start_direction_index + step_count);
                 }
             }
             None => {
                 panic!("node not found {}", current_node);
             }
         }
-        if current_node == "ZZZ" {
-            break;
+        match end.len() {
+            1 => {
+                if current_node.ends_with(end) {
+                    break;
+                }
+            }
+            _ => {
+                if current_node == end {
+                    break;
+                }
+            }
         }
     }
-    println!("{} steps", step_count);
+    if print_steps && step_count >= 5 {
+        println!(
+            "    ->{} {}",
+            current_node,
+            start_direction_index + step_count
+        );
+    }
+    (current_node.to_string(), step_count)
+}
+
+// get least common multiple of two numbers
+fn lcm(a: u64, b: u64) -> u64 {
+    let greater = a.max(b);
+    let smaller = a.min(b);
+    for i in (greater..=a * b).step_by(greater.try_into().unwrap()) {
+        if i % smaller == 0 {
+            return i;
+        }
+    }
+    panic!("no lcm found");
+}
+
+#[derive(Debug)]
+struct Ghost {
+    current_node: String,
+    step_count: u64,
+    loop_step_count: u64,
+}
+
+fn main() {
+    println!("✨ 💫 ✨ ⭐️ ✨ 💫 ✨ ✨ 💫 ✨ ⭐️ ✨ 💫 ✨ ✨ 💫");
+    println!("Hello, adventofcode.com/2023/day/8 from rust!");
+
+    let args = std::env::args().collect::<Vec<String>>();
+    let part = if args.len() > 1 {
+        args[1].parse::<u64>().unwrap()
+    } else {
+        1
+    };
+    println!("--- Part {} ---", if part == 1 { "One" } else { "Two" });
+
+    #[allow(unused_variables)]
+    let input = if part == 1 {
+        EXAMPLE_PART1
+    } else {
+        EXAMPLE_PART2
+    };
+    let input = std::fs::read_to_string("input.txt").unwrap();
+
+    let (directions, map_entries) = parse_input(&input);
+
+    if part == 1 {
+        let (_, step_count) = solve_labirynth(&directions, &map_entries, "AAA", "ZZZ", 0, true);
+        println!("steps: {}", step_count);
+    } else {
+        let mut ghosts = map_entries
+            .iter()
+            .filter(|(n, _)| n.ends_with("A"))
+            .map(|(n, _)| Ghost {
+                current_node: n.to_string(),
+                step_count: 0,
+                loop_step_count: 0,
+            })
+            .collect::<Vec<Ghost>>();
+
+        let mut max_step_count = ghosts.iter().map(|c| c.step_count).max().unwrap();
+        while ghosts
+            .iter()
+            .any(|c| c.step_count != max_step_count && c.loop_step_count == 0)
+            || ghosts.iter().all(|c| c.step_count == 0)
+        {
+            for ghost in ghosts.iter_mut() {
+                // println!("ghost: {:?} {}", ghost, max_step_count - ghost.step_count);
+                if ghost.step_count >= max_step_count && ghost.step_count > 0 {
+                    println!("skip: {:?}", ghost);
+                    continue;
+                }
+                let (new_node, additional_steps) = solve_labirynth(
+                    &directions,
+                    &map_entries,
+                    &ghost.current_node,
+                    "Z",
+                    ghost.step_count,
+                    true,
+                );
+                // if we detect a loop, we don't add step_count
+                if ghost.current_node == new_node {
+                    println!("         loop!!!");
+                    ghost.loop_step_count = additional_steps;
+                    continue;
+                }
+                ghost.step_count += additional_steps;
+                ghost.current_node = new_node;
+
+                max_step_count = max_step_count.max(ghost.step_count);
+            }
+            println!("ghosts: {:?}", ghosts);
+            println!("max_step_count: {}", max_step_count);
+        }
+
+        let first_step_counts = ghosts.first().unwrap().step_count;
+        if ghosts.iter().all(|c| c.step_count == first_step_counts) {
+            println!("steps: {}", first_step_counts);
+            return;
+        } else {
+            // we stopped because all ghosts with step_count < max_step_count are looping
+            // find least common multiple of all step counts
+            let mut all_step_counts = ghosts.iter().map(|c| c.step_count).collect::<Vec<u64>>();
+            all_step_counts.dedup();
+            all_step_counts =
+                all_step_counts
+                    .iter()
+                    .fold(vec![all_step_counts[0]], |mut acc, &c| {
+                        acc.push(lcm(acc.last().unwrap().to_owned(), c));
+                        acc
+                    });
+            let max_step_count = all_step_counts.iter().max().unwrap();
+            println!("steps: {:#?}", max_step_count);
+        }
+    }
 }
