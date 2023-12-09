00b7ffab98b01b43d4cfa2a0bc21f7c39b427ec2
patte <p@tte.io>
2023-12-09 23:15:08 +0100

    day9 part1

---
+++ day9/README.md
@@ -0,0 +1,41 @@
+# day9
+
+I asked chatGPT4 about how to think about this problem, [but it did not come up](chatGPT.txt) with a different approach than the one outlined in the problem statement (build up difference pyramids).
+
+to run:
+```bash
+cargo build --release && time target/release/day9
+```
+
+to develop:
+```bash
+cargo watch -x "run --release"
+```
+
+## Output
+### Part 1
+```
+✨ 💫 ✨ ⭐️ ✨ 💫 ✨ ✨ 💫 ✨ ⭐️ ✨ 💫 ✨ ✨ 💫
+Hello, adventofcode.com/2023/day/9 from rust!
+--- Part One ---
+histories:
+  [3, 8, 13, 18, 23, 28, 33, 38, 43, 48, 53, 58, 63, 68, 73, 78, 83, 88, 93, 98, 103]
+  [-2, 11, 29, 53, 88, 153, 309, 726, 1835, 4661, 11532, 27538, 63421, 141097, 303903, 635229, 1292012, 2564703, 4984683, 9517003, 17906238]
+  [18, 37, 70, 125, 217, 371, 619, 998, 1568, 2495, 4304, 8537, 19327, 46991, 116021, 282572, 672229, 1560456, 3541417, 7876620, 17202960]
+  [5, 3, 9, 41, 127, 305, 637, 1261, 2529, 5316, 11643, 25862, 56858, 122127, 255336, 520269, 1036193, 2023005, 3879505, 7315347, 13567333]
+  ...
+⏅ levels: 2
+⏅ levels: 16
+⏅ levels: 18
+...
+⏅ levels: 11
+⏅ levels: 5
+⏅ levels: 15
+answer: 1974232246
+0.00s user 0.00s system 9% cpu 0.071 total
+```
+
+### Part 2
+```
+
+```
---
+++ day9/src/main.rs
@@ -0,0 +1,82 @@
+/*
+https://adventofcode.com/2023/day/9
+*/
+#[allow(dead_code)]
+static EXAMPLE: &str = "\
+0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45";
+// answer:
+// 0 3 6 9 12 15 18
+// 1 3 6 10 15 21 28
+// 10 13 16 21 30 45 68
+// => 114
+
+fn parse_input(input: &str) -> Vec<Vec<i64>> {
+    input
+        .lines()
+        .map(|line| {
+            line.split_whitespace()
+                .map(|n| n.parse::<i64>().unwrap())
+                .collect::<Vec<i64>>()
+        })
+        .collect::<Vec<Vec<i64>>>()
+}
+
+fn get_diffs(history: &Vec<i64>) -> Vec<i64> {
+    history
+        .iter()
+        .enumerate()
+        .filter(|(i, _)| *i > 0)
+        .map(|(i, n)| if i > 0 { n - history[i - 1] } else { 0 })
+        .collect::<Vec<i64>>()
+}
+
+fn main() {
+    println!("✨ 💫 ✨ ⭐️ ✨ 💫 ✨ ✨ 💫 ✨ ⭐️ ✨ 💫 ✨ ✨ 💫");
+    println!("Hello, adventofcode.com/2023/day/9 from rust!");
+
+    let args = std::env::args().collect::<Vec<String>>();
+    let part = if args.len() > 1 {
+        args[1].parse::<i64>().unwrap()
+    } else {
+        1
+    };
+    println!("--- Part {} ---", if part == 1 { "One" } else { "Two" });
+
+    #[allow(unused_variables)]
+    //let input = EXAMPLE;
+    let input = std::fs::read_to_string("input.txt").unwrap();
+
+    let histories = parse_input(&input);
+    println!("histories:");
+    for history in histories.iter().take(4) {
+        println!("  {:?}", history);
+    }
+    if histories.len() > 4 {
+        println!("  ...");
+    }
+
+    let mut next_entries = Vec::new();
+    for history in histories.iter() {
+        let mut last_diff = history.clone();
+        let mut last_entry_of_diffs = vec![last_diff.iter().last().unwrap().clone()];
+
+        loop {
+            let diff = get_diffs(&last_diff).to_owned();
+            if diff.iter().all(|d| d == &0) {
+                break;
+            }
+            last_entry_of_diffs.push(diff.iter().last().unwrap().clone());
+            last_diff = diff;
+        }
+        //println!("last_diff: {:?}", last_diff);
+        //println!("last_entry_of_diffs: {:?}", last_entry_of_diffs);
+        println!("⏅ levels: {:?}", last_entry_of_diffs.len());
+
+        let next_entry = last_entry_of_diffs.iter().sum::<i64>();
+        next_entries.push(next_entry);
+    }
+    let sum_of_next_entries = next_entries.iter().sum::<i64>();
+    println!("answer: {:?}", sum_of_next_entries);
+}
